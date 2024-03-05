c6dff97066f4f21e0f649399ff984a57cb9e0673
Gaël <contact@gaelpaquien.com>
2024-03-05 23:48:53 +0100

    build(#1): environment improvments and Dart Sass implementation

--- scripts/dev/build-docker.sh
+++ scripts/dev/build-docker.sh
@@ -9,5 +9,8 @@ echo "build-docker.sh : Building App..."
 # echo "build-docker.sh : Building Database..."
 # /usr/local/bin/build-database.sh || { echo "build-docker.sh: launch of build-database.sh failed" ; exit 1; }
 
+echo "start-sass.sh: Starting Sass in the foreground..."
+/usr/local/bin/start-sass.sh || { echo "start-sass.sh: launch of start-sass.sh failed" ; exit 1; }
+
 echo "build-docker.sh: Starting Apache in the foreground..."
-apachectl -DFOREGROUND || { echo "build-docker.sh: command "apachectl -DFOREGROUND" failed" ; exit 1; }
+apachectl -DFOREGROUND || { echo "build-docker.sh: apachectl -DFOREGROUND failed" ; exit 1; }
--- scripts/dev/start-docker.sh
+++ scripts/dev/start-docker.sh
@@ -7,5 +7,5 @@ chmod +x $ROOT_PATH/scripts/dev/build-ssl-certificate.sh
 $ROOT_PATH/scripts/dev/build-ssl-certificate.sh || { echo "start.sh: launch of build-ssl-certificate.sh failed" ; exit 1; }
 
 echo "start.sh: Building Docker..."
-docker-compose build || { echo "start.sh: command "docker-compose build" failed" ; exit 1; }
-docker-compose up || { echo "start.sh: command "docker-compose up" failed" ; exit 1; }
+docker-compose build || { echo "start.sh: docker-compose build failed" ; exit 1; }
+docker-compose up || { echo "start.sh: docker-compose up failed" ; exit 1; }
