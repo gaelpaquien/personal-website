6cbf0d8486ddfc75306152460ab291c38f0a5c05
Gaël Paquien <contact@gaelpaquien.com>
2024-03-05 23:48:53 +0100

    build(#1): environment improvments and Dart Sass implementation

--- scripts/dev/build-database.sh
+++ scripts/dev/build-database.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "build-database.sh: Creating database..."
-(cd /var/www && php bin/console doctrine:database:create --if-not-exists --env=dev) || { echo "database-build.sh: doctrine:database:create failed" ; exit 1; }
+echo "build-database.sh: Creating database if not exists..."
+(cd /var/www && symfony console doctrine:database:create --if-not-exists --env=dev) || { echo "build-database.sh: doctrine:database:create failed" ; exit 1; }
 
-echo "build-database.sh: Creating schema..."
-(cd /var/www && php bin/console doctrine:schema:create --env=dev) || { echo "database-build.sh: doctrine:schema:create failed" ; exit 1; }
+echo "build-database.sh: Applying migrations..."
+(cd /var/www && symfony console doctrine:migrations:migrate --no-interaction --env=dev) || { echo "build-database.sh: doctrine:migrations:migrate failed" ; exit 1; }
 
 echo "build-database.sh: Loading fixtures..."
-echo "yes" | (cd /var/www && php bin/console doctrine:fixtures:load --env=dev) || { echo "database-build.sh: doctrine:fixtures:load failed" ; exit 1; }
+(cd /var/www && symfony console doctrine:fixtures:load --no-interaction --env=dev) || { echo "build-database.sh: doctrine:fixtures:load failed" ; exit 1; }
