66bca0f49cdd40c147149221580815fc09e17c3d
Gaël Paquien <contact@gaelpaquien.com>
2024-03-12 20:13:36 +0100

    UI/UX(#2): Integration of layouts

--- app/assets/app.js
+++ app/assets/app.js
@@ -5,5 +5,5 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/css/styles.css';
+import './styles/css/main.css';
 import './js/index.js';
