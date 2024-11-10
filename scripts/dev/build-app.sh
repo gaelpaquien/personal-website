1cb3b7ae308d95047dbe960504549406cd21739c
Gaël Paquien <contact@gaelpaquien.com>
2024-11-10 19:19:17 +0100

    Improvement Docker env

--- scripts/dev/build-app.sh
+++ scripts/dev/build-app.sh
@@ -7,9 +7,9 @@ SCRIPT_LABEL="build-app.sh"
 echo "$SCRIPT_LABEL: Starting building app..."
 execute_command "$SCRIPT_LABEL" "Clearing cache" 0 "/var/www" symfony console cache:clear --env=dev
 execute_command "$SCRIPT_LABEL" "Installing composer dependencies" 0 "/var/www" composer install
-#execute_command "$SCRIPT_LABEL" "Optimizing composer autoloader" 0 "/var/www" composer dump-autoload --optimize
-#execute_command "$SCRIPT_LABEL" "Installing importmap" 0 "/var/www" symfony console importmap:install
-#execute_command "$SCRIPT_LABEL" "Building assets" 0 "/var/www" symfony console asset-map:compile
-#execute_command "$SCRIPT_LABEL" "Warmup cache" 0 "/var/www" symfony console cache:warmup --env=dev
-#execute_command "$SCRIPT_LABEL" "Building sitemaps" 0 "/var/www" symfony console presta:sitemaps:dump public --env=dev
+#execute_command "$SCRIPT_LABEL" "Optimizing composer autoloader" 0 "/var/www/" composer dump-autoload --optimize
+#execute_command "$SCRIPT_LABEL" "Installing importmap" 0 "/var/www/" symfony console importmap:install
+#execute_command "$SCRIPT_LABEL" "Building assets" 0 "/var/www/" symfony console asset-map:compile
+#execute_command "$SCRIPT_LABEL" "Warmup cache" 0 "/var/www/" symfony console cache:warmup --env=dev
+#execute_command "$SCRIPT_LABEL" "Building sitemaps" 0 "/var/www/" symfony console presta:sitemaps:dump public --env=dev
 echo "$SCRIPT_LABEL: Building app completed"
