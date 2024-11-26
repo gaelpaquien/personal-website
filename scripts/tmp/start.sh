5300d6a77f85efd33720d8dfe2ad1dd868340319
Gaël Paquien <contact@gaelpaquien.com>
2024-11-26 23:26:15 +0100

    WIP : Fix Env

--- scripts/tmp/start.sh
+++ scripts/tmp/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-ROOT_PATH="$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")"
-SCRIPT_LABEL="start-docker.sh"
+source functions.sh
+
+ROOT_PATH="$(dirname "$(dirname "$(realpath "$0")")")"
+SCRIPT_LABEL="start.sh"
 
 echo "$SCRIPT_LABEL: Building SSL certificate..."
-chmod +x "$ROOT_PATH/scripts/dev/build-ssl-certificate.sh"
-"$ROOT_PATH/scripts/dev/build-ssl-certificate.sh" || { echo "$SCRIPT_LABEL: launch of build-ssl-certificate.sh failed" ; exit 1; }
+chmod +x "$ROOT_PATH/scripts/build-ssl-certificate.sh"
+"$ROOT_PATH/scripts/build-ssl-certificate.sh" || { echo "$SCRIPT_LABEL: launch of build-ssl-certificate.sh failed" ; exit 1; }
 
 echo "$SCRIPT_LABEL: Building Docker..."
 if ! command -v docker-compose &> /dev/null; then
@@ -15,3 +17,4 @@ fi
 
 docker-compose build || { echo "$SCRIPT_LABEL: docker-compose build failed" ; exit 1; }
 docker-compose up || { echo "$SCRIPT_LABEL: docker-compose up failed" ; exit 1; }
+execute_command "$SCRIPT_LABEL" "Start the script to building app" 0 "/usr/local/bin" ./app.sh
