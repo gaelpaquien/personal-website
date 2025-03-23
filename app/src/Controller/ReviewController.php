5a23d6c8e49267554a2631a5b440ef10ebd51913
Gaël Paquien <contact@gaelpaquien.com>
2025-03-23 18:09:53 +0100

    Refacto portfolio

---
+++ app/src/Controller/ReviewController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[\Symfony\Component\Routing\Annotation\Route('/', name: 'app_review_')]
+class ReviewController extends AbstractController
+{
+    #[Route([
+        'fr' => '/avis/creation',
+        'en' => '/reviews/create',
+    ], name: 'create', options: ['sitemap' => ['priority' => 0.8, 'changefreq' => 'monthly']])]
+    public function create(): Response
+    {
+        return $this->render('main/reviews/create.html.twig');
+    }
+}
