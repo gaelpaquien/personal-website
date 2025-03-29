75e8aea907aeb675d2ae379ec2e46c988d8e4442
Gaël Paquien <gael@plany.fr>
2025-03-29 12:29:46 +0100

    WIP: Custom input file

--- app/assets/controllers/input_file.controller.js
+++ app/assets/controllers/input_file.controller.js
@@ -1,18 +1,22 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-    static targets = ['input']
+    static targets = ['input', 'filesContainer']
     static values = {
         chooseFile: String,
-        noFileChosen: String
+        noFileChosen: String,
+        oneFileSelected: String,
+        multipleFilesSelected: String,
     }
 
     connect() {
+        this.files = new DataTransfer()
         this.customizeFileInput()
     }
 
     customizeFileInput() {
         if (!this.hasInputTarget) return
+        this.inputTarget.setAttribute('multiple', 'multiple')
 
         const wrapper = document.createElement('div')
         wrapper.className = 'custom-file-input-wrapper'
@@ -30,6 +34,11 @@ export default class extends Controller {
 
         this.inputTarget.parentNode.insertBefore(wrapper, this.inputTarget)
 
+        const filesContainer = document.createElement('div')
+        filesContainer.className = 'files-container'
+        filesContainer.setAttribute('data-input-file-target', 'filesContainer')
+        this.inputTarget.parentNode.insertBefore(filesContainer, this.inputTarget.nextSibling)
+
         this.inputTarget.style.opacity = 0
         this.inputTarget.style.position = 'absolute'
         this.inputTarget.style.pointerEvents = 'none'
@@ -38,12 +47,73 @@ export default class extends Controller {
             this.inputTarget.click()
         })
 
-        this.inputTarget.addEventListener('change', () => {
-            if (this.inputTarget.files.length > 0) {
-                fileNameLabel.textContent = this.inputTarget.files[0].name
-            } else {
-                fileNameLabel.textContent = this.noFileChosenValue
-            }
+        this.inputTarget.addEventListener('change', this.handleFileSelection.bind(this))
+
+        this.fileNameLabel = fileNameLabel
+    }
+
+    handleFileSelection() {
+        Array.from(this.inputTarget.files).forEach(file => {
+            this.files.items.add(file)
         })
+
+        this.updateInputFiles()
+
+        this.updateFilesDisplay()
+    }
+
+    updateInputFiles() {
+        this.inputTarget.files = this.files.files
+    }
+
+    updateFilesDisplay() {
+        const fileCount = this.files.files.length
+        if (fileCount > 0) {
+            if (fileCount === 1) {
+                this.fileNameLabel.textContent = `${fileCount} ${this.oneFileSelectedValue}`
+            } else {
+                this.fileNameLabel.textContent = `${fileCount} ${this.multipleFilesSelectedValue}`
+            }
+
+        } else {
+            this.fileNameLabel.textContent = this.noFileChosenValue
+        }
+
+        if (this.hasFilesContainerTarget) {
+            this.filesContainerTarget.innerHTML = ''
+
+            Array.from(this.files.files).forEach(file => {
+                const fileTag = document.createElement('div')
+                fileTag.className = 'file-tag'
+
+                const fileName = document.createElement('span')
+                fileName.className = 'file-tag-name'
+                fileName.textContent = file.name
+
+                const removeBtn = document.createElement('span')
+                removeBtn.className = 'file-tag-remove'
+                removeBtn.innerHTML = '&times;'
+                removeBtn.setAttribute('data-filename', file.name)
+                removeBtn.addEventListener('click', this.removeFile.bind(this))
+
+                fileTag.appendChild(fileName)
+                fileTag.appendChild(removeBtn)
+                this.filesContainerTarget.appendChild(fileTag)
+            })
+        }
+    }
+
+    removeFile(event) {
+        const fileName = event.currentTarget.getAttribute('data-filename')
+        const newFiles = new DataTransfer()
+
+        Array.from(this.files.files)
+            .filter(file => file.name !== fileName)
+            .forEach(file => newFiles.items.add(file))
+
+        this.files = newFiles
+
+        this.updateInputFiles()
+        this.updateFilesDisplay()
     }
 }
