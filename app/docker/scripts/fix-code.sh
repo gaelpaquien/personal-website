312189fdcf6a54cccdaeffb51d7efa920083eb60
Gaël Paquien <gael@plany.fr>
2025-05-31 22:37:03 +0200

    Improve env

--- app/docker/scripts/fix-code.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting fixing code..."
-cd /var/www && composer run-script cs-fix || exit 1
-cd /var/www && composer run-script phpcbf || exit 1
-cd /var/www && composer run-script rector || exit 1
-echo "Fixing code completed"
