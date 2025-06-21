527ba7ceaff556bb7e9f03c063876523d1f4872f
Gaël Paquien <gael@plany.fr>
2025-06-21 22:47:07 +0200

    Added recaptcha

---
+++ app/src/Validator/RecaptchaValidator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Validator;
+
+use App\Service\RecaptchaService;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class RecaptchaValidator extends ConstraintValidator
+{
+    public function __construct(
+        private RecaptchaService $recaptchaService,
+        private RequestStack $requestStack
+    ) {}
+
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof Recaptcha) {
+            throw new \InvalidArgumentException('Expected instance of ' . Recaptcha::class);
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+        if (!$request) {
+            return;
+        }
+
+        $recaptchaResponse = $request->request->get('g-recaptcha-response');
+
+        if (!$recaptchaResponse || !$this->recaptchaService->verify($recaptchaResponse, $request->getClientIp())) {
+            $this->context->buildViolation($constraint->message)
+                ->addViolation();
+        }
+    }
+}
